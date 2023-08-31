082c0baecd841cce9d314f1ba12d5f02142ba3b6
gabrielhso <gabriel.onishi@outlook.com>
2023-08-30 21:53:31 -0300

    Primeira implementação do roteiro 2

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class TokenType(Enum):
     INT = auto()
     PLUS = auto()
     MINUS = auto()
+    MULT = auto()
+    DIV = auto()
 
 class Token:
     '''
@@ -35,7 +37,7 @@ class Tokenizer:
     def select_next(self) -> None:
         '''Lê o próximo token e atualiza o atributo next'''
 
-        alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '\n', '\t', '-', '+']
+        alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '\n', '\t', '-', '+', '*', '/']
         
         next_is_blank = True
         while next_is_blank:
@@ -48,6 +50,12 @@ class Tokenizer:
                     self.position += 1
                 elif self.source[self.position] == '+':
                     self.next = Token(value='+', type=TokenType.PLUS)
+                    self.position += 1
+                elif self.source[self.position] == '*':
+                    self.next = Token(value='*', type=TokenType.MULT)
+                    self.position += 1
+                elif self.source[self.position] == '/':
+                    self.next = Token(value='/', type=TokenType.DIV)
                     self.position += 1
                 elif self.source[self.position].isdigit():
                     this_value = ""
@@ -69,40 +77,58 @@ class Parser:
     tokenizer = None
 
     @staticmethod
+    def parse_term():
+        '''
+        Consome tokens calculando termo de multiplicação/divisão
+        ''' 
+        term = 0
+        if Parser.tokenizer.next.type == TokenType.INT:
+            term = Parser.tokenizer.next.value
+            Parser.tokenizer.select_next()
+            while(Parser.tokenizer.next.type == TokenType.DIV or Parser.tokenizer.next.type == TokenType.MULT):
+                if Parser.tokenizer.next.value == '*':
+                    Parser.tokenizer.select_next()
+                    if Parser.tokenizer.next.type == TokenType.INT:
+                        term *= Parser.tokenizer.next.value
+                    else:
+                        raise ValueError(f'Caractere {Parser.tokenizer.next.value} não esperado em position = {Parser.tokenizer.position}')
+                if Parser.tokenizer.next.value == '/':
+                    Parser.tokenizer.select_next()
+                    if Parser.tokenizer.next.type == TokenType.INT:
+                        term //= Parser.tokenizer.next.value
+                    else:
+                        raise ValueError(f'Caractere {Parser.tokenizer.next.value} não esperado em position = {Parser.tokenizer.position}')
+                Parser.tokenizer.select_next()
+            return term
+        raise ValueError(f'Primeiro caractere {Parser.tokenizer.next.value} precisa ser do tipo INT, mas é do tipo {Parser.tokenizer.next.type}')
+
+    @staticmethod
     def parse_expression():
         '''
         Consome tokens do Tokenizer e analisa se a sintaxe está aderente à gramática proposta
         '''
         result = 0
-        if Parser.tokenizer.next.type == TokenType.INT:
-            result = Parser.tokenizer.next.value
-            Parser.tokenizer.select_next()
-            while(Parser.tokenizer.next.type == TokenType.MINUS or Parser.tokenizer.next.type == TokenType.PLUS):
-                if Parser.tokenizer.next.value == '+':
-                    Parser.tokenizer.select_next()
-                    if Parser.tokenizer.next.type == TokenType.INT:
-                        result += Parser.tokenizer.next.value
-                    else:
-                        raise ValueError(f'Caractere {Parser.tokenizer.next.value} não esperado em position = {Parser.tokenizer.position}')
-                if Parser.tokenizer.next.value == '-':
-                    Parser.tokenizer.select_next()
-                    if Parser.tokenizer.next.type == TokenType.INT:
-                        result -= Parser.tokenizer.next.value
-                    else:
-                        raise ValueError(f'Caractere {Parser.tokenizer.next.value} não esperado em position = {Parser.tokenizer.position}')
+        while Parser.tokenizer.next.type != TokenType.EOF:
+            result = Parser.parse_term()
+            if Parser.tokenizer.next.value == '-':
                 Parser.tokenizer.select_next()
-            return result
-        raise ValueError(f'Primeiro caractere {Parser.tokenizer.next.value} precisa ser do tipo INT, mas é do tipo {Parser.tokenizer.next.type}')
+                result -= Parser.parse_term()
+            if Parser.tokenizer.next.value == '+':
+                Parser.tokenizer.select_next()
+                result += Parser.parse_term()
+        
+        return result
     
     @staticmethod
     def run(code:str):
         Parser.tokenizer = Tokenizer(code)
         Parser.tokenizer.select_next()
         result = Parser.parse_expression()
-        if Parser.tokenizer.next.type == TokenType.EOF:
-            return result
-        else:
-            raise ValueError('Fim não esperado')
+        return result
+        # if Parser.tokenizer.next.type == TokenType.EOF:
+        #     return result
+        # else:
+        #     raise ValueError('Fim não esperado')
 
 if __name__ == '__main__':
     print(Parser.run(sys.argv[1]))
