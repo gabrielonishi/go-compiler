184cd593c93d53a30129e441ea71bec29cebf32c
gabrielhso <gabriel.onishi@outlook.com>
2023-10-24 11:35:43 -0300

    Modifica Tokenizer para aceitar var, int e string'

--- tokens.py
+++ tokens.py
@@ -56,37 +56,25 @@ class TokenType(Enum):
     '''
     EOF = auto()
     INT = auto()
-    # soma e subtração
     FIRST_ORDER_OPERATIONS = auto()
-    # PLUS = auto()
-    # MINUS = auto()
     SECOND_ORDER_OPERATIONS = auto()
-    # MULT = auto()
-    # DIV = auto()
     PARENTHESIS = auto()
     BRACKETS = auto()
-    # OPEN_PARENTHESIS = auto()
-    # CLOSE_PARENTHESIS = auto()
-    # OPEN_BRACKET = auto()
-    # CLOSE_BRACKET = auto()
     LINEFEED = auto()
     ATTRIBUTE = auto()
     PRINT = auto()
     IDENTIFIER = auto()
     NUMERIC_COMPARISON = auto()
-    # GREATERTHAN = auto()
-    # LESSERTHAN = auto()
     NOT = auto()
     BOOL_OPERATION = auto()
-    # OR = auto()
-    # AND = auto()
-    # EQUALITY = auto()
     SCANLN = auto()
     IF = auto()
     FOR = auto()
     ELSE = auto()
     SEMICOLON = auto()
-
+    STRING = auto()
+    VAR_DECLARATION = auto()
+    VAR_TYPE = auto()
 
 class Token:
     '''
@@ -102,17 +90,84 @@ class Tokenizer:
     '''
     Transforma uma sequência de caracteres em tokens
     Análise léxica do programa
-Println(x)en da expressão, alterando a posição de análise
+    Println(x) da expressão, alterando a posição de análise
     '''
 
     OPERATORS = ['-', '+', '*', '/',
                  '(', ')', '=', '\n', '&', '|', '>', '<', '!', '{', '}', ';']
-    RESERVED_KEYWORDS = ['Println', 'Scanln', 'if', 'else', 'for']
+    RESERVED_KEYWORDS = ['Println', 'Scanln', 'if', 'else', 'for', 'var', 'string', 'int']
 
     def __init__(self, source: str) -> None:
         self.source = source
         self.position = 0
         self.next = None
+
+    def handle_operators(self, next_character : str ) -> None:
+        if next_character == '-':
+            self.next = Token(value='-', type=TokenType.FIRST_ORDER_OPERATIONS)
+        elif next_character == '+':
+            self.next = Token(value='+', type=TokenType.FIRST_ORDER_OPERATIONS)
+        elif next_character == '*':
+            self.next = Token(value='*', type=TokenType.SECOND_ORDER_OPERATIONS)
+        elif next_character == '/':
+            self.next = Token(value='/', type=TokenType.SECOND_ORDER_OPERATIONS)
+        elif next_character == '(':
+            self.next = Token(value='(', type=TokenType.PARENTHESIS)
+        elif next_character == ')':
+            self.next = Token(value=')', type=TokenType.PARENTHESIS)
+        elif next_character == '\n':
+            self.next = Token(value='\n', type=TokenType.LINEFEED)
+        elif next_character == '>':
+            self.next = Token(value='>', type=TokenType.NUMERIC_COMPARISON)
+        elif next_character == '<':
+            self.next = Token(value='<', type=TokenType.NUMERIC_COMPARISON)
+        elif next_character == '!':
+            self.next = Token(value='!', type=TokenType.NOT)
+        elif next_character == '{':
+            self.next = Token(value='{', type=TokenType.BRACKETS)
+        elif next_character == '}':
+            self.next = Token(value='}', type=TokenType.BRACKETS)
+        elif next_character == ';':
+            self.next = Token(value=';', type=TokenType.SEMICOLON)
+        elif next_character == '=':
+            if self.source[self.position + 1] == '=':
+                self.next = Token(value='==', type=TokenType.NUMERIC_COMPARISON)
+                self.position += 1
+            else:
+                self.next = Token(value='=', type=TokenType.ATTRIBUTE)
+        elif next_character == '&':
+            if self.source[self.position + 1] == '&':
+                self.next = Token(value='&&', type=TokenType.BOOL_OPERATION)
+                self.position += 1
+            else:
+                raise ValueError(
+                    "ERRO EM Tokenizer.select_next(): '&' não é um operador válido. Você quis dizer '&&'?")
+        elif next_character == '|':
+            if self.source[self.position + 1] == '|':
+                self.next = Token(value='||', type=TokenType.BOOL_OPERATION)
+                self.position += 1
+            else:
+                raise ValueError(
+                    "ERRO EM Tokenizer.select_next(): '|' não é um operador válido. Você quis dizer '||'?")
+        self.position += 1
+    
+    def handle_reserved_keywords(self, this_identifier : str ) -> None:
+        if this_identifier == 'Println':
+            self.next = Token(value='Println', type=TokenType.PRINT)
+        elif this_identifier == 'Scanln':
+            self.next = Token(value='Scanln', type=TokenType.SCANLN)
+        elif this_identifier == 'if':
+            self.next = Token(value='if', type=TokenType.IF)
+        elif this_identifier == 'for':
+            self.next = Token(value='for', type=TokenType.FOR)
+        elif this_identifier == 'else':
+            self.next = Token(value='else', type=TokenType.ELSE)
+        elif this_identifier == 'var':
+            self.next = Token(value='var', type=TokenType.VAR_DECLARATION)
+        elif this_identifier == 'string':
+            self.next = Token(value='string', type= TokenType.VAR_TYPE)
+        elif this_identifier == 'int':
+            self.next = Token(value='int', type=TokenType.VAR_TYPE)
 
     def select_next(self) -> None:
         '''Lê o próximo token e atualiza o atributo next'''
@@ -121,59 +176,19 @@ Println(x)en da expressão, alterando a posição de análise
             return
         next_character = self.source[self.position]
         if next_character in Tokenizer.OPERATORS:
-            if next_character == '-':
-                self.next = Token(value='-', type=TokenType.FIRST_ORDER_OPERATIONS)
-            elif next_character == '+':
-                self.next = Token(value='+', type=TokenType.FIRST_ORDER_OPERATIONS)
-            elif next_character == '*':
-                self.next = Token(value='*', type=TokenType.SECOND_ORDER_OPERATIONS)
-            elif next_character == '/':
-                self.next = Token(value='/', type=TokenType.SECOND_ORDER_OPERATIONS)
-            elif next_character == '(':
-                self.next = Token(value='(', type=TokenType.PARENTHESIS)
-            elif next_character == ')':
-                self.next = Token(value=')', type=TokenType.PARENTHESIS)
-            elif next_character == '\n':
-                self.next = Token(value='\n', type=TokenType.LINEFEED)
-            elif next_character == '>':
-                self.next = Token(value='>', type=TokenType.NUMERIC_COMPARISON)
-            elif next_character == '<':
-                self.next = Token(value='<', type=TokenType.NUMERIC_COMPARISON)
-            elif next_character == '!':
-                self.next = Token(value='!', type=TokenType.NOT)
-            elif next_character == '{':
-                self.next = Token(value='{', type=TokenType.BRACKETS)
-            elif next_character == '}':
-                self.next = Token(value='}', type=TokenType.BRACKETS)
-            elif next_character == ';':
-                self.next = Token(value=';', type=TokenType.SEMICOLON)
-            elif next_character == '=':
-                if self.source[self.position + 1] == '=':
-                    self.next = Token(value='==', type=TokenType.NUMERIC_COMPARISON)
-                    self.position += 1
-                else:
-                    self.next = Token(value='=', type=TokenType.ATTRIBUTE)
-            elif next_character == '&':
-                if self.source[self.position + 1] == '&':
-                    self.next = Token(value='&&', type=TokenType.BOOL_OPERATION)
-                    self.position += 1
-                else:
-                    raise ValueError(
-                        "ERRO EM Tokenizer.select_next(): '&' não é um operador válido. Você quis dizer '&&'?")
-            elif next_character == '|':
-                if self.source[self.position + 1] == '|':
-                    self.next = Token(value='||', type=TokenType.BOOL_OPERATION)
-                    self.position += 1
-                else:
-                    raise ValueError(
-                        "ERRO EM Tokenizer.select_next(): '|' não é um operador válido. Você quis dizer '||'?")
-            self.position += 1
+            self.handle_operators(next_character=next_character)
         elif next_character.isdigit():
             this_value = ''
             while self.position != len(self.source) and self.source[self.position].isdigit():
                 this_value += self.source[self.position]
                 self.position += 1
             self.next = Token(value=int(this_value), type=TokenType.INT)
+        elif next_character == '"':
+            this_string = ''
+            while self.position != len(self.source) and self.source[self.position] != '"':
+                this_string += self.source[self.position]
+                self.position += 1
+            self.next = Token(value=this_string, type=TokenType.STRING)
         elif next_character.isalpha():
             this_identifier = ''
             while self.position != len(self.source) and (self.source[self.position].isalnum() or
@@ -181,16 +196,7 @@ Println(x)en da expressão, alterando a posição de análise
                 this_identifier += self.source[self.position]
                 self.position += 1
             if (this_identifier in Tokenizer.RESERVED_KEYWORDS):
-                if this_identifier == 'Println':
-                    self.next = Token(value='Println', type=TokenType.PRINT)
-                elif this_identifier == 'Scanln':
-                    self.next = Token(value='Scanln', type=TokenType.SCANLN)
-                elif this_identifier == 'if':
-                    self.next = Token(value='if', type=TokenType.IF)
-                elif this_identifier == 'for':
-                    self.next = Token(value='for', type=TokenType.FOR)
-                elif this_identifier == 'else':
-                    self.next = Token(value='else', type=TokenType.ELSE)
+                self.handle_reserved_keywords(self, this_identifier=this_identifier)
             else:
                 self.next = Token(value=this_identifier,
                                   type=TokenType.IDENTIFIER)
