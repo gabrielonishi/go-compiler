efaa83391e2d810e62e42025b7ad90b674c6114d
gabrielhso <gabriel.onishi@outlook.com>
2023-11-03 18:35:10 -0300

    Adiciona Header e Footer para o documento de saída

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
         raise ValueError("É necessário enviar um arquivo de entrada e o nome do arquivo de saída")  
           
     file = sys.argv[1]
+    output_program_name = sys.argv[2]
 
     with open(file=file, mode="r") as f:
         code = f.read()
 
-    output_program_name = sys.argv[2]
-    write.ProgramWriter.set_program_name(output_program_name)
-    write.ProgramWriter.erase_document()
+    write.ProgramWriter.start(output_program_name)
 
     clean_code = tokens.PrePro.filter(source=code)
     root = parse.Parser.run(clean_code)
     symbol_table = nodes.SymbolTable()
     root.evaluate(symbol_table=symbol_table)
+    write.ProgramWriter.end()
--- write.py
+++ write.py
@@ -4,19 +4,32 @@ class ProgramWriter():
     arquivo assembly
     '''
     
-    program_name = "program.txt"
-    
-    @staticmethod
-    def set_program_name(new_program_name:str):
-        ProgramWriter.program_name = new_program_name
+    program_name = None
+    HEADER_DOC_NAME = "program_header.txt"
+    FOOTER_DOC_NAME = "program_footer.txt"
 
-    @staticmethod
-    def erase_document():
-        with open(ProgramWriter.program_name, "w"):
-            pass
+    @staticmethod 
+    def start(output_program_name:str):
+        ProgramWriter.program_name = output_program_name
+
+        with open(ProgramWriter.HEADER_DOC_NAME, "r") as header_file:
+            header_content = header_file.read()
+        
+        with open(ProgramWriter.program_name, "w") as program_file:
+            program_file.write(header_content)
 
     @staticmethod
     def write_line(code:str):
         with open(ProgramWriter.program_name, "a") as file:
             file.write(code)
             file.write("\n")
+
+    @staticmethod
+    def end():
+        with open(ProgramWriter.FOOTER_DOC_NAME, "r") as footer_file:
+            footer_content = footer_file.read()
+
+        with open(ProgramWriter.program_name, "a") as file:
+            file.write("\n")
+            file.write(footer_content)
+        
